5a64d4a3bab566ca97a4a6bd33070a455bc0149a
T0mmyYu <hoching561@gmail.com>
2025-11-18 08:52:58 +0800

    Refactor Layout component syntax

--- client/src/app/(pages)/layout.tsx
+++ client/src/app/(pages)/layout.tsx
@@ -1,18 +1,16 @@
-import type * as React from "react";
-
 import { Init } from "#/components/layout/init";
 import { Nav } from "#/components/layout/nav";
 
-export default ({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>): React.JSX.Element => {
-    return (
-        <>
-            <Nav />
-            <Init />
-            {children}
-        </>
-    );
-};
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Init />
+      <Nav />
+      {children}
+    </>
+  );
+}
